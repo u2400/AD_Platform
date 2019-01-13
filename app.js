59ce66d2c9ed9624fe154e0223d9d574c16ea1fd
HardToName <33834823+HardT0Name@users.noreply.github.com>
2019-01-13 14:44:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const Koa = require('koa');
-const router = require('./router.js');
-const port = '80';
-const app = new Koa();
+// const Koa = require('koa');
+// const router = require('./router.js');
+// const port = '80';
+// const app = new Koa();
 
-app.use(router.routes());
+// app.use(router.routes());
 
-app.listen(port);
+// app.listen(port);
 
-console.log(`app listing at port ${port}...`);
+// console.log(`app listing at port ${port}...`);
