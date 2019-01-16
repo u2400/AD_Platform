91cb88411c98f82537baeb439262a7c818c179c5
HardToName <33834823+HardT0Name@users.noreply.github.com>
2019-01-16 22:42:19 +0800

    update

--- controller/send_request.js
+++ controller/send_request.js
@@ -3,6 +3,37 @@ var Mongo = require("./model/mongodb");
 
 Mongo.on("message",(res)=>{
     console.log(res);
+//     request({
+//     method: 'PUT',
+//     preambleCRLF: true,
+//     postambleCRLF: true,
+//     uri: 'http://service.com/upload',
+//     multipart: [
+//       {
+//         'content-type': 'application/json',
+//         body: JSON.stringify({foo: 'bar', _attachments: {'message.txt': {follows: true, length: 18, 'content_type': 'text/plain' }}})
+//       },
+//       { body: 'I am an attachment' },
+//       { body: fs.createReadStream('image.png') }
+//     ],
+//     // alternatively pass an object containing additional options
+//     multipart: {
+//       chunked: false,
+//       data: [
+//         {
+//           'content-type': 'application/json',
+//           body: JSON.stringify({foo: 'bar', _attachments: {'message.txt': {follows: true, length: 18, 'content_type': 'text/plain' }}})
+//         },
+//         { body: 'I am an attachment' }
+//       ]
+//     }
+//   },
+//   function (error, response, body) {
+//     if (error) {
+//       return console.error('upload failed:', error);
+//     }
+//     console.log('Upload successful!  Server responded with:', body);
+//   })
 })
 
 var r = Mongo.start("find", [{}, {
