6011bf20724d893b06516db8ecd9b7d53d7493b6
HardToName <33834823+HardT0Name@users.noreply.github.com>
2019-01-19 14:55:08 +0800

    update

--- controller/send_request.js
+++ controller/send_request.js
@@ -1,19 +1,42 @@
 const request = require("request");
 var Mongo = require("./model/mongodb");
 
-var mod = function(){
+function F_send_request(request){
+    let req = request; //Enable Promise to accept the corresponding parameters
+    return new Promise((req)=>{
+        console.log(req.header);
+        request({
+            method: req.method,
+            
+        },
+        function (error, response, body) {
+            if (error) {
+            return console.error('upload failed:', error);
+            }
+            console.log('Upload successful!  Server responded with:', body);
+        })
+    })
+}
+
+var mod = function(data){
+    
+
     Mongo.on("message",(res)=>{
-        console.log(res);
+        for(let i of res){
+            F_send_request(i);
+        }
     })
 
-    Mongo.start("find", [{}, {
-      'projection': {
-          time: 1,
-          unixdate: 1
-      },
-      "sort": [
-          ['unixdate', 1]
-      ]
-    }]);
+    Mongo.start("find", [{
+
+    }, {
+        // 'projection': {
+        //     time: 1,
+        //     unixdate: 1
+        // },
+        "sort": [
+            ['unixdate', 1]
+        ]
+    }])
 }
 module.exports = mod;
