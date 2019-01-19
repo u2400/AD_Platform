0c8e524b7f51c74ab984dc854c1fb61e4ef6f379
HardToName <33834823+HardT0Name@users.noreply.github.com>
2019-01-19 14:58:27 +0800

    update

--- controller/send_request.js
+++ controller/send_request.js
@@ -4,22 +4,20 @@ var Mongo = require("./model/mongodb");
 function F_send_request(request){
     let req = request; //Enable Promise to accept the corresponding parameters
     return new Promise((req)=>{
-        console.log(req.header);
-        request({
-            method: req.method,
-            
-        },
-        function (error, response, body) {
-            if (error) {
-            return console.error('upload failed:', error);
-            }
-            console.log('Upload successful!  Server responded with:', body);
-        })
+        console.log(req);
+        // request({
+        //     method: req.method,
+        // },
+        // function (error, response, body) {
+        //     if (error) {
+        //     return console.error('upload failed:', error);
+        //     }
+        //     console.log('Upload successful!  Server responded with:', body);
+        // })
     })
 }
 
 var mod = function(data){
-    
 
     Mongo.on("message",(res)=>{
         for(let i of res){
@@ -39,4 +37,5 @@ var mod = function(data){
         ]
     }])
 }
+
 module.exports = mod;
