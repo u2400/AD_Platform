0d89bba78995c6e2ff9d9cd19d34be17636cd9b6
HardToName <33834823+HardT0Name@users.noreply.github.com>
2019-02-03 14:27:38 +0800

    update

--- vue-test/src/main.js
+++ vue-test/src/main.js
@@ -2,9 +2,10 @@ import Vue from "vue";
 import Button from "ant-design-vue/lib/button";
 import "ant-design-vue/dist/antd.css";
 import App from "./App";
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
 Vue.component(Button.name, Button);
-
 Vue.config.productionTip = false;
 
 new Vue({
