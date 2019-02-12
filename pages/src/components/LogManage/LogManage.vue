7792b920e11503efa04aacb07c77b8715daffbff
HardToName <33834823+HardT0Name@users.noreply.github.com>
2019-02-12 13:13:07 +0800

    up

--- pages/src/components/LogManage/LogManage.vue
+++ pages/src/components/LogManage/LogManage.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
+    <a-row :gutter="16" type="flex" justify="center">
+      <a-col :span="11">
+        <a-input v-model="filter_input"></a-input>
+      </a-col>
+      <a-col :span="6">
+        <a-button
+            type='primary'
+            @click="() => handleSearch(this.filter_input)"
+            icon="search"
+            style="width: 90px; margin-right: 8px"
+        >Search
+        </a-button>
+        <a-button
+            @click="() => handleReset()"
+            style="width: 90px"
+        >Reset
+        </a-button>
+      </a-col>
+    </a-row>
     <div style="margin-bottom: 16px">
       <span style="margin-left: 8px">
         <a-tooltip placement="top" >
@@ -73,7 +92,7 @@ for (let i = 0; i < 46; i++) {
 
     },
     "PostObj" : null,
-    "Post" : null
+    "Post" : null,
   });
 }
 
@@ -84,12 +103,85 @@ export default {
       columns,
       selectedRowKeys: [], // Check here to configure the default column
       loading: false,
+      filter_input: '',
+      unfilted_data,
+      filter: '',
+      xss_filter: function (str) {
+        let str1 = str.replace(/(?:\\\\|\\\=)/g,"");
+        if(/\=/.test(str1)){
+          this.$message.error('搜索失败,搜索条件中所有的等号必须都被转义');
+          return false;
+        }
+        let str2 = str.replace(/(\\\/|\\\\|\\\(|\\\))/g,"");
+        str2 = str2.replace(/\/.*?\//g,"");
+        if(/[^\(\)\&\|]\s*?\(.*?\)/.test(str2)){
+          this.$message.error('搜索失败,搜索条件中不得包含函数调用');
+          return false;
+        }
+        let str3 = str1.replace(/(?!\\)\(.*?(?!\\)\)/g,"");
+        if(/(?!\\)\(/.test(str3)){
+          this.$message.error("搜索失败,搜索条件中存在未闭合的'('");
+          return false;
+        }
+        if(/(?!\\)\)/.test(str3)){
+          this.$message.error("搜索失败,搜索条件中存在未闭合的')'");
+          return false;
+        }
+
+        return true;
+      },
+       computed: {
+        data: function () {
+          let local_data = [];
+          let filter = this.filter;
+          let unfilted_data = this.unfilted_data;
+
+          if(this.filter == ''){
+            local_data = unfilted_data;
+          }
+          else{
+            if(!this.xss_filter(filter)){
+              this.filter = "null";
+              return [];
+            }
+            try{
+              var rule = filter.replace(/([a-zA-Z0-9_\.]{3,20}):\/?([^/\s]*)\/?/ig,"/$2/.test(data.$1)");
+              unfilted_data.forEach(function(data){
+                let a;
+                console.log(rule);
+                eval(`if(${rule}){
+                  a = true;
+                }
+                else{
+                  a = false;
+                }`);
+                if(a){
+                  local_data.push(data);
+                }
+              });
+            }
+            catch(e){
+              this.$message.error('搜索失败,请确认语法是否正确');
+              this.filter = "null";
+              console.error(e);
+              return [];
+            }
+          }
+          return local_data;
+        }
+      },
     }
   },
   computed: {
     hasSelected() {
       return this.selectedRowKeys.length > 0
-    }
+    },
+    handleSearch (filter_input) {
+      this.filter = filter_input;
+    },
+    handleReset () {
+      this.filter = "";
+    },
   },
   methods: {
     start () {
