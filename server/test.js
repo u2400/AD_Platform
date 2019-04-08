1ee7d6450fe5dd86a4827ae985d33b5dd2e76bb1
HardToName <33834823+HardT0Name@users.noreply.github.com>
2019-04-08 19:51:25 +0800

    Update test.js

--- server/test.js
+++ server/test.js
@@ -5,11 +5,11 @@
 //         console.log(res);
 //     })
 // })()
-(async ()=>{
-    const SendResqust = require("./controller/send_request_feedback");
-    let res = await SendResqust(["find",{table_name:"341a42b5f66acfed0acdafdfd5ef7e37437b4cf3e947b7f8ad"}]);
-    console.log("test:",res);
-})()
+// (async ()=>{
+//     const SendResqust = require("./controller/send_request_feedback");
+//     let res = await SendResqust(["find",{table_name:"341a42b5f66acfed0acdafdfd5ef7e37437b4cf3e947b7f8ad"}]);
+//     console.log("test:",res);
+// })()
 
 
 
